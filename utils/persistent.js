46b01b60a510675b455b5864fd6752e4110b71a7
t-mc4 <business.basics@aleeas.com>
2023-06-04 21:18:16 -0700

    Removed redundant checksum and added better commenting

--- utils/persistent.js
+++ utils/persistent.js
@@ -10,13 +10,6 @@ export const s3 = new S3({
     signatureVersion: 'v4',
 });
 
-async function checksum(filePath) {
-    const hash = crypto.createHash('md5');
-    const data = await fs.readFile(filePath);
-    hash.update(data);
-    return hash.digest('hex');
-}
-
 // Recursively go through local directory
 const getFilesInDirectory = async (dir, fileList = []) => {
     const files = await fs.readdir(dir);
@@ -147,6 +140,7 @@ const deleteFromS3 = (bucketName, s3FilePath) => {
     return s3.deleteObject(params).promise();
 };
 
+// Function to get files from S3
 const getFilesInS3Bucket = async (s3, bucketName, s3Path, fileList = []) => {
     const s3FilesResponse = await s3
         .listObjectsV2({ Bucket: bucketName, Prefix: s3Path })
@@ -167,6 +161,7 @@ const getFilesInS3Bucket = async (s3, bucketName, s3Path, fileList = []) => {
     return fileList;
 };
 
+// Function to load files from S3 to local/server
 export const loadDataFromS3 = async (s3, bucketName, s3Path, localPath) => {
     console.log(
         `\n.\n.\n.\n\n------------\n1. BEGINNING SYNC FROM ${s3Path} TO ${localPath}\n------------`
@@ -266,34 +261,3 @@ const downloadFromS3 = async (bucketName, s3FilePath, localFilePath) => {
 const deleteFromLocal = (filePath) => {
     return fs.unlink(filePath);
 };
-
-// -------------------------------------------------------//
-// TEST THE CLOUDFLARE CONNECTION AND GET PRESIGNED LINKS //
-// ------------------------------------------------------ //
-
-// console.log(await s3.listBuckets().promise());
-
-// console.log(
-//     await s3
-//         .listObjects({ Bucket: 'airchat-persistent-vectorstorage' })
-//         .promise()
-// );
-
-// // Use the expires property to determine how long the presigned link is valid.
-// console.log(
-//     await s3.getSignedUrlPromise('getObject', {
-//         Bucket: 'airchat-persistent-vectorstorage',
-//         Key: 'contentsMap.json',
-//         Expires: 3600,
-//     })
-// );
-// // https://my-bucket-name.<accountid>.r2.cloudflarestorage.com/dog.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=<credential>&X-Amz-Date=<timestamp>&X-Amz-Expires=3600&X-Amz-Signature=<signature>&X-Amz-SignedHeaders=host
-
-// // You can also create links for operations such as putObject to allow temporary write access to a specific key.
-// console.log(
-//     await s3.getSignedUrlPromise('putObject', {
-//         Bucket: 'airchat-persistent-vectorstorage',
-//         Key: 'vectorIndex.hnsw',
-//         Expires: 3600,
-//     })
-// );
